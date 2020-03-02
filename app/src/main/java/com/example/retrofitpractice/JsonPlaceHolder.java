8cb1e8a9ea4d9a933b5a25175821326914e97c0f
Davinder Singh Kharoud <dskharoud2@gmail.com>
2020-03-01 21:58:24 -0500

    comment model

--- app/src/main/java/com/example/retrofitpractice/JsonPlaceHolder.java
+++ app/src/main/java/com/example/retrofitpractice/JsonPlaceHolder.java
@@ -9,5 +9,8 @@ import retrofit2.http.GET;
 public interface JsonPlaceHolder {
 
     @GET("posts")
-    Call< List<Post>> getPosts();
+    Call<List<Post>> getPosts();
+
+    @GET("posts/2/comments")
+    Call<List<Comment>> getComments();
 }
